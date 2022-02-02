aa8261d1872fb5f62a2e4c2ba2bb357630a86548
Anthony Gambale <ants.gambale@gmail.com>
2022-02-02 22:47:55 +1100

    changed readme

--- Interpreter.hs
+++ Interpreter.hs
@@ -13,7 +13,7 @@ eval expr env = case expr of
     Atom s       -> if (env (Atom s)) == (Atom "NULL") then (Atom s) else (env (Atom s))
     Apply e1 e2  -> case e1 of
         Lambda _ _ -> (evalLambda e1 env) (eval e2 env)
-        _          -> (eval e1 env) (eval e2 env)
+        _          -> (eval e1 env) (eval e2 env) -- This is broken
     _            -> error "Expression did not match correct lambda calculus forms."
 
 interpret :: Expression -> Expression
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Lambda-Calculus
 Interpreter for the lambda calculus with lisp-like syntax.
+
+The following example program:
+```
+(apply
+    (lambda (atom x) (atom x))
+    (atom y))
+```
+Evaluates to:
+```
+Atom "y"
+```
