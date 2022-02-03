fe6c1a9a5c652ad157c3c870ad143952be834207
Anthony Gambale <ants.gambale@gmail.com>
2022-02-03 20:43:34 +1100

    small tweaks, bug still not fixed

--- Interpreter.hs
+++ Interpreter.hs
@@ -6,15 +6,15 @@ import Data.Either
 
 evalLambda :: Env -> E -> (E -> E)
 evalLambda env expr = case expr of
-    Lambda e1 e2 -> (\argument -> eval (\atom -> if atom == e1 then argument else env atom) e2)
-    _            -> error "Non lambda expression was evaluated as a lambda."
+    Lambda p b -> (\argument -> eval (\atom -> if atom == p then argument else env atom) b)
+    _          -> error "Non lambda expression was evaluated as a lambda."
 
 eval :: Env -> E -> E
 eval env expr = case expr of
-    Atom _           -> env expr
-    Let a e1 e2      -> eval (\atom -> if atom == a then e1 else env atom) e2
-    Apply e1 e2      -> case eval env e1 of
-        Lambda p b  -> eval env ((evalLambda env (Lambda p b)) e2)
+    Atom _      -> env expr
+    Let a e1 e2 -> eval (\atom -> if atom == a then e1 else env atom) e2
+    Apply e1 e2 -> case eval env e1 of
+        Lambda p b  -> eval env ((evalLambda env (Lambda p b)) (eval env e2))
         Atom s      -> Apply (Atom s) (eval env e2)
         Apply a e   -> Apply (Apply a e) (eval env e2)
     _           -> expr
