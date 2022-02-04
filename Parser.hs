fd0b023a7cbf942ee540b78360c46aff22bafd49
Anthony Gambale <ants.gambale@gmail.com>
2022-02-05 08:49:53 +1100

    changed syntax

--- Parser.hs
+++ Parser.hs
@@ -47,7 +47,9 @@ parse :: Source -> E
 parse source
     | notElem ')' source'           = if head source' == '_' then error "Must not begin a variable name with _." else Atom (source')
     | take 5 source' == "apply"     = Apply (parse (getFirstBlock source')) (parse (getSecondBlock source'))
+    | take 1 source' == "("         = Apply (parse (getFirstBlock source')) (parse (getSecondBlock source'))
     | take 6 source' == "lambda"    = Lambda (parse (getFirstBlock source')) (parse (getSecondBlock source'))
+    | take 1 source' == "\\"        = Lambda (parse (getFirstBlock source')) (parse (getSecondBlock source'))
     | take 9 source' == "defglobal" = Defglobal (parse (getFirstBlock source')) (parse (getSecondBlock source'))
     | take 3 source' == "let"       = let name = (getFirstBlock . dropParens) (getFirstBlock source')
                                           val = (getSecondBlock . dropParens) (getFirstBlock source')
