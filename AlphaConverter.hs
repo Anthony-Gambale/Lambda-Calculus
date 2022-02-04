e3e625a90e42ad89a5f9b74a58fc7a3d0fc843be
Anthony Gambale <ants.gambale@gmail.com>
2022-02-04 20:18:17 +1100

    test passes

--- AlphaConverter.hs
+++ AlphaConverter.hs
@@ -7,11 +7,12 @@ import Syntax
 -- under the given expression.
 rename :: E -> E -> E -> E
 rename (Atom from) (Atom to) expr = case expr of
-    Atom a             -> Atom (if a == from then to else a)
-    Apply e1 e2        -> Apply (continue e1) (continue e2)
-    Lambda (Atom a) e  -> Lambda (continue (Atom a)) (continue e)
-    Let (Atom a) e1 e2 -> Let (continue (Atom a)) (continue e1) (continue e2)
-    _                  -> error "Invalid lambda calculus expression."
+    Atom a               -> Atom (if a == from then to else a)
+    Apply e1 e2          -> Apply (continue e1) (continue e2)
+    Lambda (Atom a) e    -> Lambda (continue (Atom a)) (continue e)
+    Let (Atom a) e1 e2   -> Let (continue (Atom a)) (continue e1) (continue e2)
+    Defglobal (Atom a) e -> Defglobal (continue (Atom a)) (continue e)
+    _                    -> error "Invalid lambda calculus expression."
     where
         continue = rename (Atom from) (Atom to)
 
@@ -22,14 +23,15 @@ gen times n = Atom ((replicate times '_') ++ (show n))
 -- | Short for Alpha Convert Helper
 ach :: [E] -> Int -> (Int -> E) -> E -> E
 ach used curr f expr = case expr of
-    Atom a             -> Atom a
-    Apply e1 e2        -> Apply (ach used curr f e1) (ach used curr f e2)
-    Let (Atom a) e1 e2 -> Let (Atom a) (ach used curr f e1) (ach used curr f e2)
-    Lambda (Atom a) e  -> let newAtom = f curr
-                              newUsed = newAtom : used
-                              renamedExpr = rename (Atom a) newAtom e
-                           in Lambda newAtom (ach newUsed (curr + 1) f renamedExpr)
-    _                  -> error "Invalid lambda calculus expression."
+    Atom a               -> Atom a
+    Apply e1 e2          -> Apply (ach used curr f e1) (ach used curr f e2)
+    Let (Atom a) e1 e2   -> Let (Atom a) (ach used curr f e1) (ach used curr f e2)
+    Defglobal (Atom a) e -> Defglobal (Atom a) (ach used curr f e)
+    Lambda (Atom a) e    -> let newAtom = f curr
+                                newUsed = newAtom : used
+                                renamedExpr = rename (Atom a) newAtom e
+                             in Lambda newAtom (ach newUsed (curr + 1) f renamedExpr)
+    _                    -> error "Invalid lambda calculus expression."
 
 alphaConvert = ach [] 1 (gen 1)
 
