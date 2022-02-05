2e795461840f5fe9a8305a9057835b6263a5f069
Anthony Gambale <ants.gambale@gmail.com>
2022-02-06 09:20:10 +1100

    first attempt at lazy evaluation

--- Interpreter.hs
+++ Interpreter.hs
@@ -15,9 +15,9 @@ eval env expr = case expr of
     Atom _             -> env expr
     Let (Atom a) e1 e2 -> eval (\atom -> if atom == (Atom a) then e1 else env atom) e2
     Apply e1 e2        -> case eval env e1 of
-        Lambda (Atom a) b -> eval env ((evalLambda env (Lambda (Atom a) b)) (eval env e2))
-        irreducible       -> Apply irreducible (eval env e2)
-    Lambda (Atom a) b  -> Lambda (Atom a) (eval env b)
+        Lambda (Atom a) b -> eval env ((evalLambda env (Lambda (Atom a) b)) e2)
+        irreducible       -> Apply irreducible e2
+    Lambda (Atom a) b  -> Lambda (Atom a) b
     _                  -> error "Invalid lambda calculus expression."
 
 -- | evalp for Evaluate Program
